b43d07260e7ab655e77c42f2e84f649946275674
Kristina <kristy.net.ua@mail.ru>
2021-11-30 21:58:52 +0300

    fix

--- lesson_04+/tasksHard_1.js
+++ lesson_04+/tasksHard_1.js
@@ -9,24 +9,24 @@
 // // Запросить у пользователя доход и вывести в консоль сумму налога
 
 
-// 'use strict';
+'use strict';
 
-// const sum = +prompt('Сумма', '');
-// let result;
+const sum = +prompt('Сумма', '');
+let result;
 
-// switch (!Number.isNaN(sum)) {
-//   case sum < 15000:
-//     result = sum * 0.13;
-//     console.log(result);
-//     break;
-//   case sum >= 15000 && sum < 50000:
-//     result = sum * 0.2;
-//     console.log(result);
-//     break;
-//   case sum >= 50000:
-//     result = sum * 0.3;
-//     console.log(result);
-//     break;
-//   default:
-//     console.log('Ошибка');
-// }
+switch (!Number.isNaN(sum)) {
+  case sum < 15000:
+    result = sum * 0.13;
+    console.log(result);
+    break;
+  case sum >= 15000 && sum < 50000:
+    result = sum * 0.2;
+    console.log(result);
+    break;
+  case sum >= 50000:
+    result = sum * 0.3;
+    console.log(result);
+    break;
+  default:
+    console.log('Ошибка');
+}
